732887c2c85524c4d5ad5cc2f12547eaef7e0bc1
saliou-t <zalltalla46@gmail.com>
2022-01-22 00:49:36 +0000

    Créer et importer des composants

---
+++ LesBases/VueCli/FirstProject/hello-world/src/components/Contenue.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div v-html="text"></div>
+    </div>
+</template>
+<script>
+
+   export default{
+       data() {
+           return {
+               text: '<h2>Voici le nouveau conteune de nouvel component !!! </h2>'
+           }
+       },
+   }
+
+</script>
+<style>
+</style>
